bea83897629c5128d7e2cef84816a652f709a21c
Tristan Glatard <tristan.glatard@concordia.ca>
2021-01-29 00:25:49 -0500

    Fixed nilearn install

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+pip install nilearn &>/dev/null && echo Installed Nilearn
 unset PYTHONPATH
 MINICONDA_INSTALLER_SCRIPT=Miniconda3-latest-Linux-x86_64.sh
 MINICONDA_PREFIX=/usr/local
@@ -8,7 +9,7 @@ chmod +x $MINICONDA_INSTALLER_SCRIPT
 ./$MINICONDA_INSTALLER_SCRIPT -b -f -p $MINICONDA_PREFIX &> /dev/null && echo Installed Conda
 conda install -c conda-forge -y git-annex &> /dev/null && echo Installed git-annex
 apt install netbase &>/dev/null && echo Installed apt packages
-conda install datalad datalad-osf boutiques nilearn &> /dev/null && echo Installed Python dependencies
+conda install datalad datalad-osf boutiques &> /dev/null && echo Installed DataLad and Boutiques
 
 sudo cp fslstats /bin && sudo chmod 755 /bin/fslstats && echo Installed fslstats
 git config --global user.email "example@example.org" && git config --global user.name "HIBALL Winter School container" && echo Configured Git
